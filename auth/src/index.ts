862e83be062423affd2caedb904406972799b01b
phelang.qhu@hotmail.com <phelang.qhu@gmail.com>
2021-10-08 16:56:51 +0200

    Resolve asyncronous error by importing express-async-errors to index.ts

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express'
+import 'express-async-errors'
 import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
 import { signoutRouter } from './routes/signout'
 import { signupRouter } from './routes/signup'
 import { errorHandler } from './middlewares/error-handler'
+import { NotFoundError } from './errors/not-found-error'
 const app = express()
 app.use(express.json())
 
@@ -11,6 +13,10 @@ app.use(currentUserRouter)
 app.use(signinRouter)
 app.use(signoutRouter)
 app.use(signupRouter)
+
+app.all('*', async (req, res, next) => {
+  throw new NotFoundError()
+})
 
 app.use(errorHandler)
 
