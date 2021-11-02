d660192d4b2ff22dbb2e78f0fbedb8ddb04c2c9d
phelang.qhu@hotmail.com <phelang.qhu@gmail.com>
2021-11-02 10:23:36 +0200

    Add MongoDB container and auth password hashing

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose'
+import { Password } from '../services/password'
 
 interface UserAttrs {
   email: string
   password: string
+  role: string
 }
 
 interface UserModel extends mongoose.Model<UserDoc> {
@@ -12,6 +14,7 @@ interface UserModel extends mongoose.Model<UserDoc> {
 interface UserDoc extends mongoose.Document {
   email: string
   password: string
+  role: string
 }
 
 const userSchema = new mongoose.Schema({
@@ -23,6 +26,18 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  role: {
+    type: String,
+    required: true,
+  },
+})
+
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'))
+    this.set('password', hashed)
+  }
+  done()
 })
 
 userSchema.statics.build = (attrs: UserAttrs) => {
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express'
 import { body, validationResult } from 'express-validator'
 import { RequestValidationError } from '../errors/request-validation-error'
-import {BadRequestError} from '../errors/bad-request-errors'
+import { BadRequestError } from '../errors/bad-request-error'
 
 import { User } from '../models/user'
 
@@ -23,7 +23,7 @@ router.post(
       throw new RequestValidationError(errors.array())
     }
 
-    const { email, password } = req.body
+    const { email, password, role } = req.body
 
     const existingUser = await User.findOne({ email })
 
@@ -31,7 +31,7 @@ router.post(
       throw new BadRequestError('Email in use')
     }
 
-    const user = User.build({ email, password })
+    const user = User.build({ email, password, role })
     await user.save()
 
     res.status(201).send(user)
