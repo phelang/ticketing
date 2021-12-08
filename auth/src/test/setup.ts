d3235c1cc1e68f1a5121642fd329293d83fddaff
phelang.qhu@hotmail.com <phelang.qhu@gmail.com>
2021-12-08 11:55:45 +0200

    Add unit test for signup email, password and role

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,3 +1,24 @@
 import { MongoMemoryServer } from 'mongodb-memory-server'
 import mongoose from 'mongoose'
 import { app } from '../app'
+
+let mongoServer: any
+
+beforeAll(async () => {
+  // process.env.JWT_KEY = 'abcdef'
+
+  mongoServer = await MongoMemoryServer.create()
+  const mongoUri = mongoServer.getUri()
+  await mongoose.connect(mongoUri)
+}, 99999)
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections()
+
+  collections.forEach(async (collection) => await collection.deleteMany({}))
+})
+
+afterAll(async () => {
+  // await mongoServer.stop()
+  await mongoose.disconnect()
+})
